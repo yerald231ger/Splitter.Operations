a26b7b3f99f507ded38341964531802206a3e595
Gerardo Sanchez Hernandez <yerald231ger@gmail.com>
2024-02-17 03:08:41 -0600

    Refactor function to improve performance

---
+++ Splitter.Operations.WebApi/OrderRoutes.cs
@@ -0,0 +1,17 @@
+﻿using Splitter.Operations.Infrastructure;
+
+namespace Splitter.Operations.WebApi;
+
+public static class OrderRoutes
+{
+    public static void MapOrderRoutes(this IEndpointRouteBuilder app)
+    {
+        var routeGroup = app.MapGroup("/order");
+
+        routeGroup.MapGet("/", async(IOrderRepository orderRepository) =>
+        {
+            var result = await orderRepository.GetAsync();
+            return Results.Ok(result);
+        });
+    }
+}

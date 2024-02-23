e71cdf149b35733140dc3fbe20f4ef1c1e734726
Gerardo Sanchez Hernandez <yerald231ger@gmail.com>
2024-02-23 11:03:14 -0600

    Add GetProducts and GetVouchers methods to OrderRepository

--- Splitter.Operations.Infrastructure/IOrderRepository.cs
+++ Splitter.Operations.Infrastructure/IOrderRepository.cs
@@ -4,4 +4,6 @@ namespace Splitter.Operations.Infrastructure;
 
 public interface IOrderRepository : IRepository<Order, Guid>
 {
+    Task<List<Product>?> GetProducts(Guid id);
+    Task<List<Voucher>?> GetVouchers(Guid id);
 }
--- Splitter.Operations/OrderService.cs
+++ Splitter.Operations/OrderService.cs
@@ -17,9 +17,25 @@ public class OrderService(IOrderRepository orderRepository, ISptInterface sptInt
             if (command.OrderId != null && command.OrderId != Guid.Empty)
             {
                 var order = await _orderRepository.GetByIdAsync(command.OrderId.Value);
+
+                if (order != null)
+                {
+                    if (command.WithVouchers)
+                    {
+                        var vouchers = await _orderRepository.GetVouchers(order.Id);
+                        order.Vouchers = vouchers;
+                    }
+
+                    if (command.WithProducts)
+                    {
+                        var products = await _orderRepository.GetProducts(order.Id);
+                        order.Products = products;
+                    }
+                }
+
                 return _sptInterface.CompleteGet(command.CommandId, (IEnumerable<Order>)(order is null ? [] : [order]));
             }
-            
+
             var specification = new GetByRangeDateEspecification<Order>(command.From, command.To, x => x.CreatedAt);
             var result = (IEnumerable<Order>)await _orderRepository.Filter(specification.IsSatisfiedBy);
             return _sptInterface.CompleteGet(command.CommandId, result);
